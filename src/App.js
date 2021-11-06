1060ada6962cc2abe0e7918962ec25c19507ba29
Alberto Caparrós Trigo <acaparros87@gmail.com>
2021-11-06 15:11:38 +0100

    First version of member component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from './components/Header';
+import Member from './components/Member';
 
 function App() {
   return (
     <div className='container'>
       <Header />
+      <Member name='Alberto' surname='Caparros' color='steelblue' />
+      <Member name='Juen' surname='Cruise' color='steelblue' />
     </div>
   );
 }
