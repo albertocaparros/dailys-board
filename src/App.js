589e628e2251f418cd2ee445af0c101a5b260556
Alberto Caparrós Trigo <Alberto.Caparros.Trigo@unit4.com>
2022-06-24 14:47:19 +0200

    Basic reminder on checking improvements

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import ActionContextProvider from './components/contexts/ActionContext';
 import MembersContextProvider from './components/contexts/MembersContext';
 import ActionsArea from './components/actions/ActionsArea';
 import MembersArea from './components/members/MembersArea';
+import Improvement from './components/improvements/Improvement';
 
 function App() {
   return (
     <div className='container'>
+      <Improvement></Improvement>
       <SprintContextProvider>
         <Timeline />
       </SprintContextProvider>
---
+++ src/components/improvements/Improvement.js
@@ -0,0 +1,61 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import { useEffect } from 'react';
+
+const CustomToastWithLink = () => (
+  <div>
+    <a
+      style={{ textDecoration: 'none', color: 'white' }}
+      target='_blank'
+      rel='noreferrer'
+      href='https://dev.azure.com/unit4-global/U4ERP/_queries/query/b638d1db-72b3-4a92-9c37-d293ee550c3c'>
+      It's been more than 3 days! Check your improvements
+    </a>
+  </div>
+);
+
+const Improvement = ({ message }) => {
+  useEffect(() => {
+    var lastReminder =
+      JSON.parse(localStorage.getItem('improvementReminder')) ||
+      new Date().getTime();
+    var now = new Date().getTime();
+    var dayInMs = 86400000;
+
+    if (now - lastReminder > dayInMs * 3) {
+      notify();
+      localStorage.setItem('improvementReminder', JSON.stringify(now));
+    }
+  }, []);
+
+  const notify = () =>
+    toast(CustomToastWithLink, {
+      position: 'bottom-center',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+    });
+
+  return (
+    <div>
+      <ToastContainer
+        position='bottom-center'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='dark'
+      />
+    </div>
+  );
+};
+
+export default Improvement;
