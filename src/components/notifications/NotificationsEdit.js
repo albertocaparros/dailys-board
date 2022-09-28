f0d23e6fb16f7407ccbf85dc468a0707ca2f2541
Alberto Caparrós Trigo <acaparros87@gmail.com>
2022-09-28 06:27:49 +0200

    Toast linked to notifications

--- src/components/notifications/NotificationsEdit.js
+++ src/components/notifications/NotificationsEdit.js
@@ -1,27 +1,48 @@
 import { FiSave } from 'react-icons/fi';
 import { useState } from 'react';
+import Toast from '../notifications/Toast';
 
 function NotificationsEdit() {
   let timerValues = [
+    { title: '10 seconds', value: 0.1 },
+    { title: '1 minute', value: 1 },
+    { title: '2 minutes', value: 2 },
+    { title: '5 minutes', value: 5 },
+    { title: '10 minutes', value: 10 },
     { title: '15 minutes', value: 15 },
-    { title: '30 minutes', value: 30 },
-    { title: '1 hour', value: 60 },
-    { title: '2 hours', value: 120 },
     { title: '1 day', value: 24 * 60 },
+    { title: '2 days', value: 24 * 60 * 2 },
+    { title: '3 days', value: 24 * 60 * 3 },
+    { title: '4 days', value: 24 * 60 * 4 },
+    { title: '1 week', value: 24 * 60 * 7 },
   ];
 
-  const [name, setName] = useState('');
+  const [toasts, setToasts] = useState([]);
+  const [message, setMessage] = useState('');
   const [timer, setTimer] = useState(timerValues.length - 1);
   const [url, setUrl] = useState('');
 
-  const saveNewMember = (e) => {
+  const handleSave = (e) => {
     e.preventDefault();
+
+    setToasts([
+      ...toasts,
+      { message: message, url: url, minutes: timerValues[timer].value },
+    ]);
   };
 
   return (
     <div className='md:flex md:gap-2'>
+      {toasts.map((toast) => (
+        <Toast
+          key={toast.message}
+          message={toast.message}
+          url={toast.url}
+          minutes={toast.minutes}
+        />
+      ))}
       <form
-        onSubmit={saveNewMember}
+        onSubmit={handleSave}
         className='flex flex-col p-2 my-2 border md:basis-5/12 md:flex-2 md:mx-auto md:flex-1 md:my-4'>
         <div className='flex items-center'>
           <p className='flex-1 ml-1 text-xl'>Add new notification</p>
@@ -30,8 +51,8 @@ function NotificationsEdit() {
         <input
           name='message'
           className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 my-2'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           type='text'
           placeholder='Notification message'></input>
 
@@ -56,6 +77,7 @@ function NotificationsEdit() {
           onChange={(e) => setTimer(e.target.value)}></input>
 
         <button
+          onClick={handleSave}
           type='submit'
           className='flex items-center gap-1 px-3 py-1 mx-auto text-xl font-semibold text-gray-800 bg-white border border-gray-400 rounded hover:text-teal-700 hover:shadow'>
           <FiSave />
--- src/components/notifications/Toast.js
+++ src/components/notifications/Toast.js
@@ -2,58 +2,60 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import { useEffect } from 'react';
 
-const CustomToastWithLink = () => (
-  <div>
-    <a
-      style={{ textDecoration: 'none', color: 'white' }}
-      target='_blank'
-      rel='noreferrer'
-      href='https://dev.azure.com/unit4-global/U4ERP/_queries/query/b638d1db-72b3-4a92-9c37-d293ee550c3c'>
-      It's been more than 3 days! Check your improvements
-    </a>
+const CustomToastWithLink = (message, url) => (
+  <div className='hover:cursor-default'>
+    <img
+      alt='toast'
+      style={{ width: '100px', margin: 'auto' }}
+      src='https://i.pravatar.cc/300'></img>
+    <h1 className='text-xl font-semibold text-center color:white'>{message}</h1>
+    <p className='text-center underline color:white hover:cursor-pointer'>
+      <a target='_blank' rel='noreferrer' href={url}>
+        {url}
+      </a>
+    </p>
   </div>
 );
 
-const Toast = ({ message }) => {
+const Toast = ({ message, url, minutes }) => {
   useEffect(() => {
-    let lastReminder =
-      JSON.parse(localStorage.getItem('improvementReminder')) ||
-      new Date().getTime();
-    let now = new Date().getTime();
-    let dayInMs = 86400000;
+    async function checkToast() {
+      let now = new Date().getTime();
+      let minuteInMs = 60000;
 
-    if (now - lastReminder > dayInMs * 3) {
-      notify();
-      localStorage.setItem('improvementReminder', JSON.stringify(now));
+      let lastReminder =
+        JSON.parse(localStorage.getItem('improvementReminder')) ||
+        now - minuteInMs * minutes;
+
+      if (now - lastReminder > minuteInMs * minutes) {
+        message && notify(message, url);
+        localStorage.setItem('improvementReminder', JSON.stringify(now));
+      }
     }
-  }, []);
 
-  const notify = () =>
-    toast(CustomToastWithLink, {
+    const intervalId = setInterval(() => {
+      checkToast();
+    }, 1000 * 10);
+    return () => clearInterval(intervalId);
+  }, [message, minutes, url]);
+
+  const notify = (message, url) =>
+    toast(CustomToastWithLink(message, url), {
       position: 'bottom-center',
-      autoClose: 5000,
+      autoClose: 3000,
       hideProgressBar: false,
-      closeOnClick: true,
+      newestOnTop: true,
+      closeOnClick: false,
       pauseOnHover: true,
       draggable: true,
       progress: undefined,
       theme: 'dark',
+      closeButton: false,
     });
 
   return (
     <div>
-      <ToastContainer
-        position='bottom-center'
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme='dark'
-      />
+      <ToastContainer />
     </div>
   );
 };
